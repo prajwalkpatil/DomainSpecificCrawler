c2374a682ea8fc9b0217f84401303e6823f05cce
unknown <hubballisiddaraj@gmail.com>
2022-12-15 14:18:45 +0530

    yes no remove from search terms in only_string.py

--- only-string.py
+++ only-string.py
@@ -28,7 +28,7 @@ source_directory = './JSONFiles_withID/'
 all_files = os.listdir(source_directory)
 
 # input ids from the user
-input_ids = {"5761", "5308"}
+input_ids = {"1915", "5400", "5000"}
 temp_ids = input_ids
 item_values = {}
 temp_list = []
@@ -47,7 +47,7 @@ def append_to_dictionary(l):
 # file is each .json files
 for file in all_files:
     with open(f"{source_directory}/{file}", 'r') as f:
-        # data is complete json file in form of dictionary
+        # data is complete json file in form of dictionary  
         data = json.load(f)
 
         # each id is an item in json file
@@ -76,9 +76,25 @@ for id in item_values:
             temp = value.split(',')
             item_values[id].remove(value)
             item_values[id].extend(temp)
+            
         # remove yes and nos
-        if value == 'Yes' or value == 'yes' or value == 'No' or value == 'no':
-            item_values[id].remove(value)
+        print(">>>>>>>>>>>>>>>>>>")
+        try:
+            item_values[id].remove('Yes')
+        except:
+            pass
+        try:
+            item_values[id].remove('yes')
+        except:
+            pass
+        try:
+            item_values[id].remove('No')
+        except:
+            pass
+        try:
+            item_values[id].remove('no')
+        except:
+            pass           
         # remove if the value is only numeric
         if value.isnumeric():
             item_values[id].remove(value)
@@ -89,10 +105,11 @@ for id in item_values:
         i = i+1
 
 # Selecting n random words from each id list and appending to search_items list
-n = 3        # n random words are selected from each list
+n = 4        # n random words are selected from each list
 search_items = []
 for id in item_values:
-    temp_list = random.sample(item_values[id], n)
+    # temp_list = random.sample(item_values[id], n)
+    temp_list = random.sample(item_values[id], len(item_values[id]))
     search_items.append(temp_list)
 print(search_items)
 print('\n')
@@ -104,6 +121,7 @@ for i in range(len(search_items)):
     sets.append(i)
 
 sets = findsubsets(sets, 2)  # change number to get more terms
+print(sets)
 
 for i in range(len(sets)):
     for j in range(n):
@@ -116,24 +134,68 @@ for i in range(len(search_terms)):
     print(search_terms[i])
 
 
-page_range = []
-no_of_pages = 1
-for i in range(0, no_of_pages):
-    page_range.append(i * 5)
 
-all_links = []
-for term in search_terms:
-    for i in page_range:
-        params['start'] = i
-        params['q'] = term
-        html = requests.get("https://www.google.com/search",
-                            headers=headers, params=params, timeout=20)
-        soup = BeautifulSoup(html.text, 'lxml')
-        for result in soup.select('.tF2Cxc'):
-            link = result.select_one('.yuRUbf a')['href']
-            # print(link)
-            all_links.append(link)
-            append_to_dictionary(link)
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> 1
+# page_range = []
+# no_of_pages = 1          #change to get more links
+# for i in range(0, no_of_pages):
+#     page_range.append(i * 5)
+
+# all_links = []
+# for term in search_terms:
+#     for i in page_range:
+#         params['start'] = i
+#         params['q'] = term
+#         html = requests.get("https://www.google.com/search",
+#                             headers=headers, params=params, timeout=20)
+#         soup = BeautifulSoup(html.text, 'lxml')
+#         for result in soup.select('.tF2Cxc'):
+#             link = result.select_one('.yuRUbf a')['href']
+#             print(link)
+#             all_links.append(link)
+#             append_to_dictionary(link)
+
+
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> 2
+# from spellchecker import SpellChecker
+# spell = SpellChecker
+
+# from urllib.request import urlopen
+# from urllib.error import HTTPError
+# import nltk
+# from bs4 import BeautifulSoup as bs
+# import requests
+# from googlesearch import search
+# for term in search_terms:
+#     for link in search(term,lang = "en", tld="co.in", num=5, stop=5, pause=20):
+#         print(link)
+#         append_to_dictionary(link)
+
+
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> 3
+# import re
+# import json
+# import nltk
+# import string
+# from datetime import datetime
+# from googlesearch import search   
+# from urllib.request import urlopen
+# from collections import Counter
+# from bs4 import BeautifulSoup
+# from nltk.tokenize import word_tokenize, sent_tokenize
+
+# def get_links(keyword):
+#     global link_dictionary
+#     for j in search(keyword, tld="co.in", num = 1, stop = 1, pause=60): 
+#         #Check if its forbidden URL
+#         # if not is_ul(j):
+#             #Append the link to the array
+#         print(j)
+#         append_to_dictionary(j)
+# for term in search_terms:
+#     get_links(term)
+
+
 
 print(">>>>>>>>>>>>>>")
 print(link_dictionary)
